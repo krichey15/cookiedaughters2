8a11a4a89647ecdefafad571e44bffbf6a55eb8f
Kyle Richey <richey.kyle.i@gmail.com>
2021-03-04 21:30:14 -0800

    add slider to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+// import { Link } from 'gatsby'
 import Helmet from 'react-helmet';
 
 import SEO from '~/components/seo';
+import ImageSlider from '../components/ImageSlider';
+import { SliderData } from '../components/ImageSlider/SliderData';
 
 const IndexPage = () => (
   <>
@@ -9,6 +12,8 @@ const IndexPage = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     </Helmet>
+    <ImageSlider slides={SliderData} />
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </>
 )
 
