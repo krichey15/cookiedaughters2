c98d152644a3941c0ae025096ad297f2c05beec5
Kyle Richey <richey.kyle.i@gmail.com>
2021-03-08 08:52:25 -0800

    banner 150%

--- src/components/Banner/BannerElements.js
+++ src/components/Banner/BannerElements.js
@@ -1,12 +1,11 @@
 import styled from '@emotion/styled'
 
 export const BannerContainer = styled.div`
-    width: 100%;
     display: flex;
     justify-content: center;
 `
 
 export const BannerImage = styled.img`
-    width: 100%;
+    width: 150%;
     margin-bottom: 50px;
 `
