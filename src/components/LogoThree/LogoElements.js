8adf87ee922321007960f20b72256e19e33259c9
Kyle Richey <richey.kyle.i@gmail.com>
2021-03-09 10:43:59 -0800

    Logo Flex-start test on safari and iOS

--- src/components/LogoThree/LogoElements.js
+++ src/components/LogoThree/LogoElements.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 export const LogoContainer = styled.div`
     display: flex;
     justify-content: center;
+    align-items: flex-start;
     padding-bottom: 2%;
     padding-top: 5%;
 `
@@ -10,5 +11,4 @@ export const LogoContainer = styled.div`
 export const StyledLogoImage = styled.img`
     width: 100%;
     max-width: 1000px;
-    height: auto;
 `
