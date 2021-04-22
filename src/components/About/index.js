b86b3991d727fc113a817fe14bba9394a318c7e3
Kyle Richey <richey.kyle.i@gmail.com>
2021-04-21 20:23:20 -0700

    margin on about photo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,7 @@ const About = () => {
             <h2>A Little About Me</h2>
 
             <div>
-                <div className="float-left w-1/2 md-max:w-2/3 sm-max:w-full mr-8">
+                <div className="float-left w-1/2 md-max:w-2/3 sm-max:w-full mr-8 mt-7 md-max:mt-5">
                     <img src={Family} alt='family' className='w-full' />
                 </div>
                 <div>
