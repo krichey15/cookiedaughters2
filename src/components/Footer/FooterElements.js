ff19bb88847687a69a7c3d0fabd7bf4f260ad10a
Kyle Richey <richey.kyle.i@gmail.com>
2021-10-06 08:21:30 -0700

    shop and footer

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -5,3 +5,6 @@ export const FooterP = styled.p`
     font-family: 'BenchNine', sans-serif;
     font-size: clamp(.5rem, 1.5vw, .75rem);
 `
+export const IconDiv = styled.div`
+    font-size: clamp(3.5rem, 1.5vw, 3.25rem);
+`
